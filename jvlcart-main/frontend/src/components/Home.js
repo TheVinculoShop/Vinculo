814e4e023ece987e4b536c41ceee409c8facb7c0
Karthikeyan <137636670+karthi049@users.noreply.github.com>
2024-07-25 12:52:49 +0530

    Pulling from branch\

--- jvlcart-main/frontend/src/components/Home.js
+++ jvlcart-main/frontend/src/components/Home.js
@@ -4,47 +4,90 @@ import { getProducts } from "../actions/productActions";
 import Loader from "./layouts/Loader";
 import MetaData from "./layouts/MetaData";
 import Product from "./product/Product";
-import  {toast} from 'react-toastify';
+import { toast } from 'react-toastify';
 import Pagination from 'react-js-pagination';
 
-export  default function Home(){
+export default function Home() {
     const dispatch = useDispatch();
-    const {products, loading, error, productsCount, resPerPage} =    useSelector((state) => state.productsState)
+    const { products, loading, error, productsCount, resPerPage } = useSelector((state) => state.productsState)
     const [currentPage, setCurrentPage] = useState(1);
- 
-    const setCurrentPageNo = (pageNo) =>{
+    const [category, setCategory] = useState('');
 
+    const setCurrentPageNo = (pageNo) => {
         setCurrentPage(pageNo)
-       
     }
 
-    useEffect(()=>{
-        if(error) {
-            return toast.error(error,{
+    useEffect(() => {
+        if (error) {
+            return toast.error(error, {
                 position: toast.POSITION.BOTTOM_CENTER
             })
         }
-        dispatch(getProducts(null, null, null, null, currentPage)) 
-    }, [error, dispatch, currentPage])
+        dispatch(getProducts(null, null, null, category, currentPage))
+    }, [error, dispatch, currentPage, category])
 
+    const handleCategoryClick = (selectedCategory) => {
+        setCategory(selectedCategory);
+        setCurrentPage(1);
+    }
 
     return (
         <Fragment>
-            {loading ? <Loader/>:
+             {/* Categories */}
+<center>
+    <div className="container mt-5">
+        <div className="row">
+            <div className="col-12 mb-5">
+                <h1 id="products_heading">Categories</h1>
+
+                <div className="image-container">
+                    <img 
+                        src="/images/girlscategory_img.jpg" 
+                        alt="Girls Category"
+                        onClick={() => handleCategoryClick('girls')}
+                    />
+                    <div class="label">Girls</div>
+                    </div>
+
+                    <div  className="image-container">
+                    <img 
+                        src="/images/boyscategory_img.jpeg" 
+                        alt="Boys Category"
+                        onClick={() => handleCategoryClick('boys')}
+                    />
+                      <div class="label">Boys</div>
+
+                    </div>
+
+                    <div  className="image-container">
+                    <img 
+                        src="/images/infantcategory_img.jpeg" 
+                        alt="Infants Category"
+                        onClick={() => handleCategoryClick('infants')}
+                    />
+                    <div class="label">Infant</div>
+                </div>
+            </div>
+        </div>
+    </div>
+</center>
+            {loading ? <Loader /> :
                 <Fragment>
                     <MetaData title={'Buy Best Products'} />
-                    <h1 id="products_heading">Latest Products</h1>
+                  <center><h1 id="products_heading">Latest Products</h1></center>  
+                    
+
+
                     <section id="products" className="container mt-5">
                         <div className="row">
-                            { products && products.map(product => (
-                                <Product col={3} key={product._id}  product={product}/>
+                            {products && products.map(product => (
+                                <Product col={3} key={product._id} product={product} />
                             ))}
-                        
                         </div>
                     </section>
-                    {productsCount > 0 && productsCount > resPerPage?
-                    <div className="d-flex justify-content-center mt-5">
-                           <Pagination 
+                    {productsCount > 0 && productsCount > resPerPage ?
+                        <div className="d-flex justify-content-center mt-5">
+                            <Pagination
                                 activePage={currentPage}
                                 onChange={setCurrentPageNo}
                                 totalItemsCount={productsCount}
@@ -54,10 +97,10 @@ export  default function Home(){
                                 lastPageText={'Last'}
                                 itemClass={'page-item'}
                                 linkClass={'page-link'}
-                           />     
-                    </div> : null }
+                            />
+                        </div> : null}
                 </Fragment>
-           }
+            }
         </Fragment>
     )
 }
--- jvlcart-main/frontend/src/components/product/Product.js
+++ jvlcart-main/frontend/src/components/product/Product.js
@@ -1,27 +1,37 @@
 import { Link } from 'react-router-dom';
+import { Carousel } from 'react-bootstrap';
 
 export default function Product ({product, col}) {
     return (
         <div className={`col-sm-12 col-md-6 col-lg-${col} my-3`}>
             <div className="card p-3 rounded">
-                {product.images.length > 0 &&
-                <img
-                className="card-img-top mx-auto"
-                src={product.images[0].image}
-                alt={product.name}
-                />}
+                {product.images && product.images.length > 0 ? (
+                    <Carousel pause='hover'>
+                        {product.images.map(image => (
+                            <Carousel.Item key={image._id}>
+                                <img
+                                    className="d-block w-100"
+                                    src={image.image}
+                                    alt={product.name}
+                                />
+                            </Carousel.Item>
+                        ))}
+                    </Carousel>
+                ) : (
+                    <p>No images available</p>
+                )}
                 <div className="card-body d-flex flex-column">
-                <h5 className="card-title">
-                    <Link to={`/product/${product._id}`}>{product.name}</Link>
-                </h5>
-                <div className="ratings mt-auto">
-                    <div className="rating-outer">
-                    <div className="rating-inner" style={{width: `${product.ratings/ 5 * 100}%` }}></div>
+                    <h5 className="card-title">
+                        <Link to={`/product/${product._id}`}>{product.name}</Link>
+                    </h5>
+                    <div className="ratings mt-auto">
+                        <div className="rating-outer">
+                            <div className="rating-inner" style={{width: `${product.ratings/ 5 * 100}%` }}></div>
+                        </div>
+                        <span id="no_of_reviews">({product.numOfReviews} Reviews)</span>
                     </div>
-                    <span id="no_of_reviews">({product.numOfReviews} Reviews)</span>
-                </div>
-                <p className="card-text">${product.price}</p>
-                <Link to={`/product/${product._id}`} id="view_btn" className="btn btn-block">View Details</Link>
+                    <p className="card-text">${product.price}</p>
+                    <Link to={`/product/${product._id}`} id="view_btn" className="btn btn-block">View Details</Link>
                 </div>
             </div>
         </div>
