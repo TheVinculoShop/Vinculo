24b7c8d2fbf9d77233bce11cf2e50d1cb33196d9
vijayvs <vijayvs6383@gmail.com>
2024-07-29 21:10:54 -0700

    sd

--- jvlcart-main/frontend/src/components/Boys.js
+++ jvlcart-main/frontend/src/components/Boys.js
@@ -6,6 +6,28 @@ import MetaData from "./layouts/MetaData";
 import Product from "./product/Product";
 import { toast } from 'react-toastify';
 import Pagination from 'react-js-pagination';
+import styled from 'styled-components';
+
+const ProductCard = styled.div`
+    display: flex;
+    justify-content: center;
+    width: 50%;
+    padding: 10px;
+    box-sizing: border-box;
+
+    @media (min-width: 768px) {
+        width: 25%;
+    }
+
+    @media (max-width: 767px) {
+        width: 50%;
+    }
+`;
+
+const Row = styled.div`
+    display: flex;
+    flex-wrap: wrap;
+`;
 
 export default function BoysProducts() {
     const dispatch = useDispatch();
@@ -32,11 +54,13 @@ export default function BoysProducts() {
                     <MetaData title={'Boys Products'} />
                     <h1 id="products_heading">Boys Products</h1>
                     <section id="products" className="container mt-5">
-                        <div className="row">
+                        <Row>
                             {products && products.map(product => (
-                                <Product col={3} key={product._id} product={product} />
+                                <ProductCard key={product._id}>
+                                    <Product col={3} product={product} />
+                                </ProductCard>
                             ))}
-                        </div>
+                        </Row>
                     </section>
                     {productsCount > 0 && productsCount > resPerPage ?
                         <div className="d-flex justify-content-center mt-5">
